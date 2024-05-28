ac494af953cb3e2c616b07401dcb7543939dacb4
Musenmy <120939558+Musenmy@users.noreply.github.com>
2024-05-28 13:58:47 +0900

    Update localeWarn.js

--- localeWarn.js
+++ localeWarn.js
@@ -4,6 +4,7 @@ window.onload = function () {
     const currentUrl = window.location.href;
     console.log(userLocale, currentUrl);
     if (!userLocale.match(/ja/) && !currentUrl.match(/en/)) {
+      //日本で英語見てるとき出す
       const messageElement = document.getElementById("message");
       const localeView = document.getElementById("localeView");
       const redirectLink = document.getElementById("redirect-link");
@@ -12,6 +13,19 @@ window.onload = function () {
         englishUrl = currentUrl.replace(/\/ja\//, "/en/");
       else englishUrl = "en/index.html";
       redirectLink.href = englishUrl;
+      localeView.innerHTML = `Your locale is ${userLocale}.`;
+      messageElement.style.display = "block";
+    }
+    if (userLocale.match(/ja/) && currentUrl.match(/en/)) {
+      //英語で日本見てるとき出す
+      const messageElement = document.getElementById("message");
+      const localeView = document.getElementById("localeView");
+      const redirectLink = document.getElementById("redirect-link");
+      let japaneseUrl;
+      if (currentUrl.match(/\/en\//))
+        japaneseUrl = currentUrl.replace(/\/en\//, "/ja/");
+      else japaneseUrl = "index.html";
+      redirectLink.href = japaneseUrl;
       localeView.innerHTML = `Your locale is ${userLocale}.`;
       messageElement.style.display = "block";
     }
